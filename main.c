9a53f70d1d2390d0c9238ea5b929e99bd3eea5d9
Timmcclymont <48046209+Timmcclymont@users.noreply.github.com>
2019-04-03 18:23:18 +1100

    Create main.c
    First git commit for ENG1003 Assignment 1

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  ENG1003 Ass 1
+//
+//  Created by Tim on 3/4/19.
+//  Copyright © 2019 Tim McClymont. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+// This is hopefully the first successful git commit... But probably not......
